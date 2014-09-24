f0e808dc43df3abbf8644c52a701be4029689aec
singular1ty <singular1ty@outlook.com>
2014-09-24 12:16:47 +1000

    Infinite Tweepy Stream

--- Chitter/chitter.py
+++ Chitter/chitter.py
@@ -1,13 +1,57 @@
 import os, os.path
-
 import cherrypy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
+# OUR TWITTER KEYS
+ckey = 'TfbXbyPI2hF6IEpNtrzGBxXET'
+csecret = 'Qtb1vPdM7YSqcXKAdQkZ15MSuXYUzrH9Piv5ddBi6uF2gES3Fh'
+atoken = '615738366-ZPmyGEWqxVNkW2UFLSmH7HadACsM2kupw1Gf6QAR'
+asecret = 'oWOi5LppyKm5ApmCQoHpKMfzARcstinukMLmuuiaRl31E'
+
+#Tweets array
+tweets = []
+
+# Simple Class for Listening to
+# the stream.
+class listener(StreamListener):
+    def on_data(self, data):
+        tweets.append(data)
+        #print tweets.pop(0)
+        return True
+
+    def on_error(self, status):
+        print status
+
+# Set up the core features, but don't stream yet.
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+
+# The Chitter Web SErver
 class Chitter(object):
+	runOnce = False
 	
 	@cherrypy.expose
 	def index(self):
    		f = open('public/index.html', 'r')
    		return f.read()
+   		
+   	@cherrypy.expose
+   	def prepare(self):
+   		twitterStream = Stream(auth, listener())
+		twitterStream.filter(track=["car"])
+		return "Streaming"
+   		
+   	@cherrypy.expose
+   	def stream(self):
+   		try:   	
+   			return tweets.pop(0)
+		except:
+			pass
+		
+   		
 
 if __name__ == '__main__':
 	conf = {
---
+++ README.md
@@ -0,0 +1,13 @@
+#Chitter
+
+A CherryPy Twitter Stream by singular1ty94.
+
+#Install
+
+You'll need to run `pip install cherrypy` and `pip install tweepy`.
+
+Sometimes you'll run into issues with Tweepy, you *must* use **Python 2.x**, and if you receive odd messages about modules not importing, you might be forced to do a clean install.
+
+#Running
+
+Just run `python chitter.py`
